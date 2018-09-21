21dbb8443d8d2fcd88b8d15e68830599537be445
tgray017 <tgray017@gmail.com>
2018-09-21 22:11:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     array.join(" and ")
   else
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
   end
 end
