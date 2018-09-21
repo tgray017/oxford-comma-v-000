b96c35a3fc8ece0242dee5aa29241897855ec27b
tgray017 <tgray017@gmail.com>
2018-09-21 22:09:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   case array.length
   when 1
     array
-  when 
+  when 2
+    array.join("and ")
+  else
   array[-1] = "and #{array[-1]}"
   array.join(", ")
+  end
 end
